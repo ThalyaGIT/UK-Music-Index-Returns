edbfe144016a3fed5f690826c9023e3564e2ab2d
ThalyaGIT <limmingshan@gmail.com>
2024-05-27 19:27:13 +0100

    - gathered valence data

--- data-ingestion/scripts/_03_process_data_list_unique_track_id.py
+++ data-ingestion/scripts/_03_process_data_list_unique_track_id.py
@@ -18,7 +18,7 @@ def process_data_list_unique_track_id(input_file_path):
     output_file_path = os.path.join(os.path.dirname(input_file_path), '03_processed_data_list_unique_track_id.csv')
 
     # Save the unique track IDs to a CSV file
-    unique_track_ids_df = unique_track_ids_df.head(500) ## remove later tech debt
+    #unique_track_ids_df = unique_track_ids_df.head(500) ## remove later tech debt
     unique_track_ids_df.to_csv(output_file_path, index=False)
 
     print(f'Unique Spotify Track IDs have been stored in {output_file_path}')
